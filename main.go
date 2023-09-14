57bd3fb43a8f97afb32dc94bb5cb44e418baa0c8
UndeadTokenArt <undeadtokenart@gmail.com>
2023-09-14 14:11:35 -0700

    Added the template for forum, and fixed main.go order of execution so that it starts the server AFTER getting the routes handled

--- main.go
+++ main.go
@@ -6,14 +6,17 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/undeadtokenart/myCRM/customerDB"
 )
 
 func main() {
+
 	// Initialize Gin
 	router := gin.Default()
 	// Serve static files from the "static" directory
 	router.Static("/static", "./static")
+	// define the location of tmpl files
 	router.LoadHTMLGlob("templates/*")
 
 	// Initialize the database
@@ -29,11 +32,6 @@ func main() {
 	// Auto-migrate the database to create the Customer table
 	customerDB.MyDataBase.AutoMigrate(customerClient)
 
-	// Start the Gin server
-	port := ":8080"
-	fmt.Printf("Server is running on port %s\n", port)
-	router.Run(port)
-
 	// Define your routes and handlers here
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "base.tmpl", gin.H{
@@ -41,12 +39,24 @@ func main() {
 		})
 	})
 
+	// For all intensive purposes, I was using this to test the gin.H passing of data
 	router.GET("/index", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "base.tmpl", gin.H{
-			"title": "Main website",
+			"Message": "Welcome",
 		})
 	})
 
+	// Saving these for once the form is ready
 	router.POST("/customers", customerDB.CreateCustomer)
 	router.GET("/customers/:id", customerDB.GetCustomer)
+
+	router.GET("/newCustomerForm", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "NewCustomerForm.tmpl", gin.H{
+			"Message": "Please tell me about the new Client",
+		})
+	})
+	// Start the Gin server
+	port := ":8080"
+	fmt.Printf("Server is running on port %s\n", port)
+	router.Run(port)
 }
